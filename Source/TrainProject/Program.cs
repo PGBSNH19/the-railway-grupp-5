c167e78a6156b13328be0a3ba6771cff90405e24
AndreMorad <56093049+AndreMorad@users.noreply.github.com>
2020-03-09 13:12:36 +0100

    @andremorad

---
+++ Source/TrainProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrainProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
