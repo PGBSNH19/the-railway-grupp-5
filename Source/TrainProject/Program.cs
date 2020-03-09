a60c1d190700c03760fe108dbc7a43a6e21e001e
AndreMorad <56093049+AndreMorad@users.noreply.github.com>
2020-03-09 14:32:00 +0100

    @andremorad

--- Source/TrainProject/Program.cs
+++ Source/TrainProject/Program.cs
@@ -9,4 +9,33 @@ namespace TrainProject
             Console.WriteLine("Hello World!");
         }
     }
+
+    class Train
+    {
+        int id = 0;
+        string name;
+        int maxSpeed = 0;
+        bool oprated;
+
+
+    }
+
+    class Switch
+    {
+        bool turnRight;
+
+    }
+
+    class LevelCrossing
+    {
+        bool open;
+    }
+
+    class Schedule
+    {
+        int traindId { get; set; }
+        int stationId { get; set; }
+        TimeSpan departureTime { get; set; }
+        TimeSpan arrivalTime { get; set; }
+    }
 }
