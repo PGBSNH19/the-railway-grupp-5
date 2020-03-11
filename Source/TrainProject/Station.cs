98e1e693994444cde3c8b03c3d718d51d8cf6ef6
singelmamma <55433931+singelmamma@users.noreply.github.com>
2020-03-11 08:29:44 +0100

    @singelmamma

--- Source/TrainProject/Station.cs
+++ Source/TrainProject/Station.cs
@@ -1,4 +1,9 @@
-﻿namespace TrainProject
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace TrainProject
 {
     public class Station
     {
@@ -13,6 +18,23 @@
             id = int.Parse(convert[0]);
             stationName = convert[1];
             endStation = bool.Parse(convert[2]);
+        }
+    }
+
+    class StationList
+    {
+        public List<Station> InitAvailableStations()
+        {
+            List<Station> AllStations = new List<Station>();
+
+            string[] station = File.ReadAllLines(Program.StationFilePath);
+
+            foreach (string item in station)
+            {
+                AllStations.Add(new Station(item));
+            }
+            return AllStations;
+
         }
     }
 }
