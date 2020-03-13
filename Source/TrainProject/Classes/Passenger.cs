eb00567771683123e27b99ccfc6b6d14c31dc08e
singelmamma <55433931+singelmamma@users.noreply.github.com>
2020-03-13 13:50:37 +0100

    @singelmamma

--- Source/TrainProject/Classes/Passenger.cs
+++ Source/TrainProject/Classes/Passenger.cs
@@ -3,14 +3,14 @@ using System.IO;
 
 namespace TrainProject
 {
-    public class Passengers
+    public class Passenger
     {
         public int id { get; }
         public string name { get; }
 
-        public Passengers(string passengerList)
+        public Passenger(string passengerList)
         {
-            string[] convert = passengerList.Split(",");
+            string[] convert = passengerList.Split(";");
             id = int.Parse(convert[0]);
             name = convert[1];
         }
@@ -18,14 +18,14 @@ namespace TrainProject
 
     public class PassengerList
     {
-        public List<Passengers> InitAllPassengers()
+        public List<Passenger> InitAllPassengers()
         {
-            List<Passengers> allPassengers = new List<Passengers>();
+            List<Passenger> allPassengers = new List<Passenger>();
             string[] passengers = File.ReadAllLines(Program.PassengersFilePath);
 
             foreach (string item in passengers)
             {
-                allPassengers.Add(new Passengers(item));
+                allPassengers.Add(new Passenger(item));
             }
 
             return allPassengers;
--- Source/TrainProject/Program.cs
+++ Source/TrainProject/Program.cs
@@ -14,6 +14,7 @@ namespace TrainProject
         public static List<Schedule> scheduleList;
         public static List<Train> trainList;
         public static List<Station> stationList;
+        public static List<Passenger> passengerList { get; set; }
         public static TimeSpan timer;
  
 
@@ -21,16 +22,15 @@ namespace TrainProject
         {
 
             CreateDatabase();
-
-            var trainPlaner1 = new TrainPlaner(trainList, 2).FollowSchedule(scheduleList);
-            var trainPlaner2 = new TrainPlaner(trainList, 3).FollowSchedule(scheduleList);
+            var trainPlaner1 = new TrainPlaner(trainList, 2).FollowSchedule(scheduleList).AddPassengers(passengerList);
+            var trainPlaner2 = new TrainPlaner(trainList, 3).FollowSchedule(scheduleList).AddPassengers(passengerList);
 
             CreateTrainPlaner testTrainPlaner1 = new CreateTrainPlaner(trainPlaner1);
             testTrainPlaner1.trainThread = new Thread(() => testTrainPlaner1.Drive(testTrainPlaner1));
 
             CreateTrainPlaner testTrainPlaner2 = new CreateTrainPlaner(trainPlaner2);
             testTrainPlaner2.trainThread = new Thread(() => testTrainPlaner2.Drive(testTrainPlaner2));
-           
+
             testTrainPlaner2.trainThread.Start();
             testTrainPlaner1.trainThread.Start();
             
@@ -48,6 +48,9 @@ namespace TrainProject
 
             StationList SList = new StationList();
             stationList = SList.InitAvailableStations();
+
+            PassengerList createPassengerList = new PassengerList();
+            passengerList = createPassengerList.InitAllPassengers();
         }
 
         public static void StartTimer() 
@@ -58,7 +61,7 @@ namespace TrainProject
             {
                 Console.WriteLine(timer);
                 timer += addMin;
-                Thread.Sleep(200);
+                Thread.Sleep(300);
             }
         }
 
@@ -66,14 +69,17 @@ namespace TrainProject
         {
             Train train { get; }
             List<Schedule> trainSchedules { get; set; }
+            List<Passenger> trainPassenger { get; set; }
 
             IControlRoom FollowSchedule(List<Schedule> schedules);
+            IControlRoom AddPassengers(List<Passenger> passengers);
         }
 
         public class TrainPlaner : IControlRoom
         {
             public Train train { get; }
             public List<Schedule> trainSchedules { get; set; }
+            public List<Passenger> trainPassenger { get; set; }
 
             public TrainPlaner(List<Train> trainList, int check)
             {
@@ -85,51 +91,72 @@ namespace TrainProject
                 trainSchedules = schedules.Where(x => x.traindId == train.id).ToList();
                 return this;
             }
+            
+            public IControlRoom AddPassengers(List<Passenger> passengers)
+            {
+                trainPassenger = new List<Passenger>();
+                Random rnd = new Random();
+                int random = rnd.Next(0, passengerList.Count);
+                for(int i = 0; i < random; i++) 
+                {
+                    trainPassenger.Add(passengers[i]);
+                }
+                passengerList.RemoveRange(0, random);
+                return this;
+            }
         }
 
         public class CreateTrainPlaner
         {
             public Train train { get; }
             public List<Schedule> trainSchedules { get; }
+            public List<Passenger> passengers { get; set; }
             public Thread trainThread;
+           
 
             public CreateTrainPlaner(IControlRoom test)
             {
                 this.train = test.train;
                 this.trainSchedules = test.trainSchedules;
+                this.passengers = test.trainPassenger;
             }
 
             public void Drive(CreateTrainPlaner driveTest)
             {
                 bool check = false;
                 string station;
-                while(check == false) {
+                
+                
+                while (check == false) {
                     
                     if(driveTest.trainSchedules[0].departureTime == timer.ToString())
-                    {
-                        
+                    { 
                         station = stationList.Where(p => p.id == driveTest.trainSchedules[0].stationId).ToList().Select(p => p.stationName).First();
-                        Console.WriteLine($"{driveTest.train.name} leaving {station}");
-                        Thread.Sleep(300);
+                        Console.WriteLine($"{driveTest.train.name} leaving {station} and {driveTest.passengers.Count} passanger(s) aboard the train");
+                        Thread.Sleep(350);
                     }
+
                     if(driveTest.trainSchedules[1].arrivalTime == timer.ToString()) 
                     {
                         station = stationList.Where(p => p.id == driveTest.trainSchedules[1].stationId).ToList().Select(p => p.stationName).First();
-                        Console.WriteLine($"{driveTest.train.name} arrived to {station}");
-                        Thread.Sleep(300);
-
+                        Random rnd = new Random();
+                        int random = rnd.Next(0, passengers.Count);
+                        Console.WriteLine($"{driveTest.train.name} arrived to {station} and {random} passenger(s) got off the train");
+                        passengers.RemoveRange(0, random);
+                        Thread.Sleep(350);
                     }
-                    if(driveTest.trainSchedules[1].departureTime == timer.ToString())
+
+                    if (driveTest.trainSchedules[1].departureTime == timer.ToString())
                     {
                         station = stationList.Where(p => p.id == driveTest.trainSchedules[1].stationId).ToList().Select(p => p.stationName).First();
                         Console.WriteLine($"{driveTest.train.name} leaving {station}");
-                        Thread.Sleep(300);
+                        Thread.Sleep(350);
                     }
 
                     if (driveTest.trainSchedules[2].arrivalTime == timer.ToString())
                     {
                         station = stationList.Where(p => p.id == driveTest.trainSchedules[2].stationId).ToList().Select(p => p.stationName).First();
-                        Console.WriteLine($"{driveTest.train.name} arrived to its end station {station}");
+                        Console.WriteLine($"{driveTest.train.name} arrived to its end station {station} and {passengers.Count} passenger(s) got off the train");
                         check = true;
                     }
                 }
