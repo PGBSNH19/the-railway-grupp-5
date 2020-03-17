55bff66f10f9006029bb501fdf9e0f9542edabaa
AndreMorad <56093049+AndreMorad@users.noreply.github.com>
2020-03-17 13:03:58 +0100

    @andremorad

--- Source/TrainProject/Program.cs
+++ Source/TrainProject/Program.cs
@@ -19,15 +19,66 @@ namespace TrainProject
 
         public static void Main(string[] args)
         {
+            //CreateDatabase();
+
+            //var trainPlaner1 = new TrainPlaner(trainList, 2).FollowSchedule(scheduleList).AddPassengers(passengerList).CrossoverControll();
+            //var trainPlaner2 = new TrainPlaner(trainList, 3).FollowSchedule(scheduleList).AddPassengers(passengerList).CrossoverControll();
+
+            //CreateTrainPlaner testTrainPlaner1 = new CreateTrainPlaner(trainPlaner1);
+            //CreateTrainPlaner testTrainPlaner2 = new CreateTrainPlaner(trainPlaner2);
+
+            //StartTimer(testTrainPlaner1, testTrainPlaner2);
+
             CreateDatabase();
 
             var trainPlaner1 = new TrainPlaner(trainList, 2).FollowSchedule(scheduleList).AddPassengers(passengerList).CrossoverControll();
+
             var trainPlaner2 = new TrainPlaner(trainList, 3).FollowSchedule(scheduleList).AddPassengers(passengerList).CrossoverControll();
 
-            CreateTrainPlaner testTrainPlaner1 = new CreateTrainPlaner(trainPlaner1);
-            CreateTrainPlaner testTrainPlaner2 = new CreateTrainPlaner(trainPlaner2);
+            CreateTrainPlaner train1 = new CreateTrainPlaner(trainPlaner1);
+            CreateTrainPlaner train2 = new CreateTrainPlaner(trainPlaner2);
+            new Thread(() =>
+            {
+                TimeSpan addMin = TimeSpan.FromMinutes(1);
+                timer = new TimeSpan(10, 15, 00);
+                for (int i = 0; i < 62; i++)
+                {
+                    Console.WriteLine(timer);
+                    if (train1.trainSchedules[0].departureTime == timer.ToString())
+                    {
+                        train1.trainThread = new Thread(() => train1.Drive1(train1));
+                        train1.trainThread.Start();
+                        train1.trainThread.Join();
+                    }
+                    if (train1.trainSchedules[1].arrivalTime == timer.ToString())
+                    {
+                        train1.trainThread = new Thread(() => train1.Drive2(train1));
+                        train1.trainThread.Start();
+                        train1.trainThread.Join();
+                    }
+                    if (train1.trainSchedules[1].departureTime == timer.ToString())
+                    {
+                        train1.trainThread = new Thread(() => train1.Drive3(train1));
+                        train1.trainThread.Start();
+                        train1.trainThread.Join();
+                    }
+                    if (train1.trainSchedules[2].arrivalTime == timer.ToString())
+                    {
+                        train1.trainThread = new Thread(() => train1.Drive4(train1));
+                        train1.trainThread.Start();
+                        train1.trainThread.Join();
+                    }
+                    //if (train2.trainSchedules[0].departureTime == timer.ToString())
+                    //{
+                    //    train2.trainThread = new Thread(() => train2.Drive(train2));
+                    //    train2.trainThread.Start();
+                    //}
 
-            StartTimer(testTrainPlaner1, testTrainPlaner2);
+                    timer += addMin;
+                    Thread.Sleep(300);
+                    Console.WriteLine("----------------");
+                }
+            }).Start();
         }
 
         public static void CreateDatabase()
@@ -45,29 +96,29 @@ namespace TrainProject
             passengerList = createPassengerList.InitAllPassengers();
         }
 
-        public static void StartTimer(CreateTrainPlaner train1, CreateTrainPlaner train2)
-        {
-            string station;
-            TimeSpan addMin = TimeSpan.FromMinutes(1);
-            timer = new TimeSpan(10, 15, 00);
-            for (int i = 0; i < 62; i++)
-            {
-                Console.WriteLine(timer);
-                if (train1.trainSchedules[0].departureTime == timer.ToString())
-                {
-                    train1.trainThread = new Thread(() => train1.Drive(train1));
-                    train1.trainThread.Start();
-                }
-                if(train2.trainSchedules[0].departureTime == timer.ToString())
-                {
-                    train2.trainThread = new Thread(() => train2.Drive(train2));
-                    train2.trainThread.Start();
-                }
-                timer += addMin;
-                Thread.Sleep(300);
-                Console.WriteLine("----------------");
-            }
-        }
+        //public static void StartTimer(CreateTrainPlaner train1, CreateTrainPlaner train2)
+        //{
+        //    string station;
+        //    TimeSpan addMin = TimeSpan.FromMinutes(1);
+        //    timer = new TimeSpan(10, 15, 00);
+        //    for (int i = 0; i < 62; i++)
+        //    {
+        //        Console.WriteLine(timer);
+        //        if (train1.trainSchedules[0].departureTime == timer.ToString())
+        //        {
+        //            train1.trainThread = new Thread(() => train1.Drive(train1));
+        //            train1.trainThread.Start();
+        //        }
+        //        if(train2.trainSchedules[0].departureTime == timer.ToString())
+        //        {
+        //            train2.trainThread = new Thread(() => train2.Drive(train2));
+        //            train2.trainThread.Start();
+        //        }
+        //        timer += addMin;
+        //        Thread.Sleep(300);
+        //        Console.WriteLine("----------------");
+        //    }
+        //}
 
         public interface IControlRoom
         {
@@ -77,7 +128,9 @@ namespace TrainProject
             List<TimeSpan> openCloseCrossover { get; set; }
 
             IControlRoom FollowSchedule(List<Schedule> schedules);
+
             IControlRoom AddPassengers(List<Passenger> passengers);
+
             IControlRoom CrossoverControll();
         }
 
